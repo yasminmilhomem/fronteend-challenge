e690bf6311e8639fb3801028dfda4fe639f09470
yasminmilhomem <yasminmilhomem1517@gmail.com>
2023-04-10 11:36:53 -0300

    comentario

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import "./Profile.css";
+import { GeoAlt, Heart, Star, Envelope, Link, Building, People, Twitter } from 'react-bootstrap-icons';
 
 function Profile() {
   const { username } = useParams();
   const [user, setUser] = useState(null);
   const [repos, setRepos] = useState([]);
   const [stars, setStars] = useState([]);
+  
   useEffect(() => {
     fetch(`https://api.github.com/users/${username}`)
       .then((response) => response.json())
@@ -35,38 +37,44 @@ function Profile() {
   }
 
   return (
+    <>
     <div className='loadedClass'>
       <div className='leftBar'>
         <img src={user.avatar_url} alt={user.login} />
         <div className='nameGroup'>
-        <h1>{user.name}</h1>
-        <h1>{user.login}</h1>
+        <h2>{user.name}</h2>
+        <h3>@{user.login}</h3>
         </div>
         <p>{user.bio}</p>
+
         <div className='numbersGroup'>
-        <p>Followers: {user.followers}</p>
-        <p>Following: {user.following}</p>
-        <p>Starred Repos: {stars}</p>
+        <p><People />Followers: {user.followers}</p>
+        <p><Heart />Following: {user.following}</p>
+        <p>< Star/>Starred Repos: {stars}</p>
         </div>
+
         <div className='infoGroup'>
-        <p>Organization: {user.company}</p>
-        <p>Location: {user.location}</p>
+        <p><Building />Organization: {user.company}</p>
+        <p><GeoAlt />Location: {user.location}</p>
+        <p><Envelope />Email: {user.email}</p>
         <p></p>
-        <p>Link: <a href={user.html_url}>{user.html_url}</a></p>
-        <p>Twitter: {user.twitter_username}</p>
-        <p>Voltar</p>
+        <p><Link />Link: <a href={user.blog}>{user.blog}</a></p>
+        <p><Twitter />Twitter: {user.twitter_username}</p>
+        <button>Voltar</button>
         </div>
       </div>
       <div className='rightBar'>
       <h2>Repositories:</h2>
       <ul>
         {repos.map((repo) => (
-          <li key={repo.id}>{repo.name} - {repo.stargazers_count} stars</li>
+          <li key={repo.id}>{repo.name} - {repo.stargazers_count} stars - Last updated: {repo.updated_at} </li>
         ))}
       </ul>
       </div>
     </div>
-
+     
+      <img src="https://raw.githubusercontent.com/makasi-tech/fronteend-challenge/main/tela_perfil.PNG" alt="texto réi" />
+      </>
   );
 }
 
